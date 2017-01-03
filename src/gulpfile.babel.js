1aeb9e8452bbef9d770a9dcd512ff2ac240ca27b
parker mccurley <parkermccurlz@gmail.com>
2017-01-03 12:12:17 -0500

    Setup babel & gulp

---
+++ src/gulpfile.babel.js
@@ -0,0 +1,41 @@
+import gulp from 'gulp';
+import del from 'del';
+import babel from 'gulp-babel';
+// import eslint from 'gulp-eslint';
+import { exec } from 'child_process';
+
+const paths = {
+  gulpFile: './gulpfile.babel.js',
+  app: './app/**/*.js',
+  libDir: './lib/'
+};
+
+gulp.task('clean', () =>
+  del(paths.libDir)
+);
+
+// gulp.task('lint', () => {
+//  gulp.src([paths.gulpFile, paths.app])
+//    .pipe(eslint())
+//    .pipe(eslint.format())
+//    .pipe(eslint.failAfterError())
+// });
+
+gulp.task('build', ['clean'], () =>
+  gulp.src(paths.app)
+    .pipe(babel())
+    .pipe(gulp.dest(paths.libDir))
+);
+
+gulp.task('main', ['build'], (callback) => {
+  exec(`node ${paths.libDir}`, (error, stdout) => {
+    console.log(stdout);
+    return callback(error);
+  });
+});
+
+gulp.task('watch', () => {
+  gulp.watch(paths.app, ['main']);
+});
+
+gulp.task('default', ['watch', 'main']);
--- src/gulpfile.js
+++
@@ -1,4 +0,0 @@
-import gulp from 'gulp';
-import del from 'del';
-import babel from 'gulp-babel';
-import eslint from 'gulp-eslint';
